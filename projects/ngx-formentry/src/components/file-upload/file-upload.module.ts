a709996af46341ec8c281b15032455576bf4d548
madvincy <eugenekandie@gmail.com>
2019-06-09 12:10:13 +0300

    file uploader fix

--- projects/ngx-formentry/src/components/file-upload/file-upload.module.ts
+++ projects/ngx-formentry/src/components/file-upload/file-upload.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FormsModule } from '@angular/forms';
-import { NgxFileUploaderModule } from 'ngx-file-uploader-ampath';
+import { NgxFileUploaderModule } from 'ngx-file-uploader-ampath2';
+
 import { SharedModule } from '../../shared.module';
 // import { SelectModule } from 'ng2-select/ng2-select';
 import { SelectModule } from '../../components/select/select.module';
 import { RemoteFileUploadComponent } from './file-upload.component';
+import { WebcamModule } from 'ngx-webcam';
 
 @NgModule({
-    imports: [CommonModule, SelectModule, FormsModule, NgxFileUploaderModule, SharedModule],
+    imports: [CommonModule, SelectModule, FormsModule, SharedModule, WebcamModule, NgxFileUploaderModule],
     exports: [RemoteFileUploadComponent],
     declarations: [RemoteFileUploadComponent],
     providers: [],
